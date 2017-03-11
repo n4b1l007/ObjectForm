f94693126cc3e3242bf5343a15dd226d399059f4
jafrul_sadek@yahoo.com <Nabil@076870>
2017-03-11 22:41:25 +0600

    select2

---
+++ ObjectForm/Attribute/ParentPropertyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ObjectForm.Attribute
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ParentPropertyesAttribute : System.Attribute
+    {
+        private readonly string _parents;
+
+        public ParentPropertyesAttribute(string parents)
+        {
+            _parents = parents;
+        }
+    }
+}
